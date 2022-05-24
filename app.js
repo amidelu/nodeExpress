353707e3c143b4695aaa7c81ec8ae1c52622ac39
amidelu <delowar_gtc@yahoo.com>
2022-05-24 23:27:52 +0600

    template engine pug added for shop page

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const http = require ('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRouter);
+app.use('/admin', adminData.router);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,16 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views','add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
